2628a70be95aa4d0e189a584d2e1bb8e0254ff67
Veryard <kingard999@gmail.com>
2026-09-22 17:30:55 +0700

    Update soundEffect.jsx

--- app/soundEffect.jsx
+++ app/soundEffect.jsx
@@ -4,10 +4,13 @@ import { useEffect } from 'react';
 
 export default function SoundEffect() {
   useEffect(() => {
+    const audio = new Audio('/assets/click.wav');
+    audio.preload = 'auto';
+
     const handleGlobalClick = () => {
-      const audio = new Audio('/assets/click.wav');
-      audio.volume = 0.2;
-      audio.play().catch(() => {});
+      const sound = audio.cloneNode();
+      sound.volume = 0.2;
+      sound.play().catch(() => {});
     };
 
     window.addEventListener('click', handleGlobalClick);
